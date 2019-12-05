21575404be1dca14b79cf3739482d46c9d3ccd3a
smbh <smb_h@yahoo.com>
2019-12-05 15:05:25 +0330

    preprocessing sepration

---
+++ LSTM neural network/data_preprocessing.py
@@ -0,0 +1,29 @@
+from .normalizers import normalise_min_max, normalise_zero_base
+from .data_splitter import train_test_split
+
+
+
+# extract data of windows - size 5
+def extract_window_data(df, window_len=5, zero_base=True):
+    window_data = []
+    for idx in range(len(df) - window_len):
+        tmp = df[idx: (idx + window_len)].copy()
+        if zero_base:
+            tmp = normalise_zero_base(tmp)
+        window_data.append(tmp.values)
+    return np.array(window_data)
+
+# prepare the data in a format to be later fed into the neural network
+def prepare_data(df, target_col, window_len=10, zero_base=True, test_size=0.2):
+    train_data, test_data = train_test_split(df, test_size=test_size)
+    X_train = extract_window_data(train_data, window_len, zero_base)
+    X_test = extract_window_data(test_data, window_len, zero_base)
+    y_train = train_data[target_col][window_len:].values
+    y_test = test_data[target_col][window_len:].values
+    if zero_base:
+        y_train = y_train / train_data[target_col][:-window_len].values - 1
+        y_test = y_test / test_data[target_col][:-window_len].values - 1
+
+    return train_data, test_data, X_train, X_test, y_train, y_test
+
+
--- LSTM neural network/src.py
+++ LSTM neural network/src.py
@@ -8,7 +8,6 @@ import seaborn as sns
 from sklearn.metrics import mean_absolute_error
 from .data_splitter import train_test_split
 from .plot import line_plot
-from .normalizers import normalise_min_max, normalise_zero_base
 
 
 endpoint = 'https://min-api.cryptocompare.com/data/histoday'
@@ -34,28 +33,6 @@ pd.plotting.register_matplotlib_converters()
 line_plot(train[target_col], test[target_col], 'training', 'test', title='')
 
 
-# extract data of windows - size 5
-def extract_window_data(df, window_len=5, zero_base=True):
-    window_data = []
-    for idx in range(len(df) - window_len):
-        tmp = df[idx: (idx + window_len)].copy()
-        if zero_base:
-            tmp = normalise_zero_base(tmp)
-        window_data.append(tmp.values)
-    return np.array(window_data)
-
-# prepare the data in a format to be later fed into the neural network
-def prepare_data(df, target_col, window_len=10, zero_base=True, test_size=0.2):
-    train_data, test_data = train_test_split(df, test_size=test_size)
-    X_train = extract_window_data(train_data, window_len, zero_base)
-    X_test = extract_window_data(test_data, window_len, zero_base)
-    y_train = train_data[target_col][window_len:].values
-    y_test = test_data[target_col][window_len:].values
-    if zero_base:
-        y_train = y_train / train_data[target_col][:-window_len].values - 1
-        y_test = y_test / test_data[target_col][:-window_len].values - 1
-
-    return train_data, test_data, X_train, X_test, y_train, y_test
 
 
 # buid neural network model
