957dcab22ac3e94659d91b914fa140d963dc1c6d
smbh <smb_h@yahoo.com>
2019-12-05 15:12:49 +0330

    crawler sepration

---
+++ LSTM neural network/crawler.py
@@ -0,0 +1,21 @@
+import json
+import requests
+import pandas as pd
+
+
+endpoint = 'https://min-api.cryptocompare.com/data/histoday'
+api_key = "fbd094f08b216fbd27e436c1134c34aeac9cf6b823f425ce4e67c43d322b7081"
+res = requests.get(endpoint + '?fsym=BTC&tsym=USD&limit=500&api_key=' + api_key)
+
+
+
+# print(res.content)
+hist = pd.DataFrame(json.loads(res.content)['Data'])
+hist = hist.set_index('time')
+hist.index = pd.to_datetime(hist.index, unit='s')
+
+# print(hist.head(5))
+
+def crawl():
+    return hist
+
--- LSTM neural network/src.py
+++ LSTM neural network/src.py
@@ -1,27 +1,14 @@
-import json
-import requests
 import numpy as np
 import pandas as pd
 from sklearn.metrics import mean_absolute_error
+from .crawler import crawl
 from .data_splitter import train_test_split
 from .plot import line_plot
 from .data_preprocessing import prepare_data
 from .lstm_neural_net import build_lstm_model
 
 
-endpoint = 'https://min-api.cryptocompare.com/data/histoday'
-api_key = "fbd094f08b216fbd27e436c1134c34aeac9cf6b823f425ce4e67c43d322b7081"
-res = requests.get(endpoint + '?fsym=BTC&tsym=USD&limit=500&api_key=' + api_key)
-
-
-
-# print(res.content)
-hist = pd.DataFrame(json.loads(res.content)['Data'])
-hist = hist.set_index('time')
-hist.index = pd.to_datetime(hist.index, unit='s')
-target_col = 'close'
-
-# print(hist.head(5))
+hist = crawl()
 
 # split data
 train, test = train_test_split(hist, test_size=0.2)
@@ -29,6 +16,7 @@ train, test = train_test_split(hist, test_size=0.2)
 
 pd.plotting.register_matplotlib_converters()
 
+target_col = 'close'
 line_plot(train[target_col], test[target_col], 'training', 'test', title='')
 
 
