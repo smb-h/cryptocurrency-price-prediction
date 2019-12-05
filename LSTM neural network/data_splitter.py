dd60adffca78954728f08ad5cfe73d9b97771097
smbh <smb_h@yahoo.com>
2019-12-05 14:55:59 +0330

    update data splitter

---
+++ LSTM neural network/data_splitter.py
@@ -0,0 +1,11 @@
+
+
+
+# split data into trainingset and test set
+def train_test_split(df, test_size=0.2):
+    split_row = len(df) - int(test_size * len(df))
+    train_data = df.iloc[:split_row]
+    test_data = df.iloc[split_row:]
+    return train_data, test_data
+
+
